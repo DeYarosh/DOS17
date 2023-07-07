a79753d4ee9a994c5ff497ce75b75e188fa06eeb
DeYarosh <deyarosh98@gmail.com>
2023-07-07 23:34:25 +0300

    Revert to 339304b

--- MyBashScripts/AutoOMZ.sh
+++ MyBashScripts/AutoOMZ.sh
@@ -4,16 +4,16 @@ set -e  #Stop the script if something goes wrong
 sudo apt install -y zsh     #ZSH installation
 echo "==== Installing of ZSH was successful! =====" 
 
-sudo chsh -s $(which zsh)    #Set the ZSH as default shell
+chsh -s $(which zsh)    #Set the ZSH as default shell
 echo "==== Now ZSH has become your default shell! ====="
 
-sudo yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y  #OhMyZsh installation
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y  #OhMyZsh installation
 echo "==== Installing of OhMyZsh was successful! ====="
 
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-sudo git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-sudo git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 echo "==== Downloading of plugins was successful! ====="
 
 old_plugins_string="plugins=(git)"
